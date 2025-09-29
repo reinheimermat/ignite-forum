79386afed710edd9e697d109851b42605b347b07
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-09-29 14:25:49 -0300

    feat: add end-to-end tests for Create Account and Authenticate Controllers

---
+++ src/infra/http/controllers/authenticate-controller.e2e-spec.ts
@@ -0,0 +1,43 @@
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { hash } from 'argon2'
+import request from 'supertest'
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+
+describe('Authenticate Controller (e2e)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: await hash('password123'),
+      },
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'john@example.com',
+      password: 'password123',
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({
+      access_token: expect.any(String),
+    })
+  })
+})
--- src/infra/http/controllers/create-account.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,38 @@
-test('1 Plus 1', () => {
-  expect(1 + 1).toBe(2)
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+
+describe('Create Account Controller (e2e)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      email: 'test@example.com',
+      name: 'Test User',
+      password: 'password123',
+    })
+
+    expect(response.status).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: { email: 'test@example.com' },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
 })
